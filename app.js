755bdfc872f32b0975dfb2c97973582ecd587f65
phenicieaaron <phenicieaaron@yahoo.com>
2023-10-09 22:29:13 -0500

    connected to mongo db

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const express = require('express')
 require('dotenv').config()
+const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = (process.env.PORT || 3000)
-const {ObjectID} = require('mongodb')
+const { ObjectId } = require('mongodb')
+const port = (process.env.PORT || 5500)
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI;
-
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,89 +19,97 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-//run().catch(console.dir);
 
-async function cxnDB(){
 
-  try{
-    client.connect; 
-    const collection = client.db("aarons-papa-database").collection("dev-profiles");
-    // const collection = client.db("papa").collection("dev-profiles");
-    const result = await collection.find().toArray();
-    //const result = await collection.findOne(); 
-    console.log("cxnDB result: ", result);
-    return result; 
-  }
-  catch(e){
-      console.log(e)
-  }
-  finally{
-    client.close; 
-  }
-}
+
 app.get('/', async (req, res) => {
 
-  let result = await cxnDB().catch(console.error); 
+  client.connect;
+  let mongoResult = await client.db("cuddly-palm-quebec").collection("dev_profiles").find().toArray();
+  console.log(mongoResult);
+  res.render('index', {
+    profileData: mongoResult
+  })
+})
 
-  // console.log("get/: ", result);
+app.post('/updateProfile', async (req, res) => {
 
-  res.send("here for a second: " + result[0].name)
-  //res.render('index', {  peopleData : result })
+  try {
+    //get the new dev name
+    console.log("body: ", req.body)
+    console.log("user Name: ", req.body.devName)
+
+    client.connect;
+    const collection = client.db("cuddly-palm-quebec").collection("dev_profiles");
+
+    // put it into mongo
+    let result = await collection.findOneAndUpdate(
+      { _id: new ObjectId(req.body.devId) },
+      { $set: { devName: req.body.devName } })
+      .then(result => {
+        console.log(result);
+        res.redirect('/');
+      })
+      .catch(error => console.error(error))
+
+
+  }
+  finally {
+    //client.close()
+  }
 })
 
 
+app.post('/insertProfile', async (req, res) => {
 
-//   let result = await cxnDB().catch(console.error); 
-//   // res.send("here for a second: " +)
+  try {
+    //get the new dev name
+    console.log("body: ", req.body)
+    console.log("user Name: ", req.body.devName)
 
-//   // console.log("get/: ", result);
+    client.connect;
+    const collection = client.db("cuddly-palm-quebec").collection("dev_profiles");
 
-//   res.render('index', {  peopleData : result })
-// })
+    // put it into mongo
+    let result = await collection.insertOne(
+      { devName: req.body.newDevName })
+      .then(result => {
+        console.log(result);
+        res.redirect('/');
+      })
+      .catch(error => console.error(error))
 
 
-
-let myVariableServer = 'soft coded data'
-
-app.get('/aaron', function (req, res) {
-  res.render('index', {'myVariableClient' : myVariableServer} );
+  }
+  finally {
+    //client.close()
+  }
 })
 
-app.post('/postClientData', function (req, res) {
- 
-  console.log("body: ", req.body);
-  console.log("user name:" , req.body.userName);
-  // console.log("params: ", req.params['userName']);
-  
-  // myVariableServer = "now!!! new we\'ve got new stuff"
-  myVariableServer = req.body.userName
+app.post('/deleteProfile', async (req, res) => {
 
-  res.render('index', {'myVariableClient' : myVariableServer} );
+  try {
+    //get the new dev name
+    console.log("body: ", req.body)
+    console.log("user Name: ", req.body.devName)
+
+    client.connect;
+    const collection = client.db("cuddly-palm-quebec").collection("dev_profiles");
+
+    // put it into mongo
+    let result = await collection.findOneAndDelete(
+      { _id: new ObjectId(req.body.devId) })
+      .then(result => {
+        console.log(result);
+        res.redirect('/');
+      })
+      .catch(error => console.error(error))
+
+
+  }
+  finally {
+    //client.close()
+  }
 })
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World From Express!')
-// })
-
-// app.get('/whatever', function (req, res) {
-//   res.sendFile(__dirname + '/index.html');
-// })
-
-
-
-
-// app.listen(3000)
-
-app.listen(port, () => console.log(`Server is running... on ${port}`));
+app.listen(port, () => console.log(`Server is running...on ${port}`));
